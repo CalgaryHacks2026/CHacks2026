380591cfdf2b64c94be54827c05ba2f655155174
nsriv531 <nsriv531@mtroyal.ca>
2026-02-14 18:14:39 -0700

    made some modificatiosn to the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,13 +26,6 @@ export default function Home() {
   </div>
 
   {/* removed nav */}
-
-  <a
-    href="#signup"
-    className="inline-flex items-center justify-center rounded-full border border-zinc-200 bg-white px-4 py-2 text-sm font-medium text-zinc-900 shadow-sm transition hover:border-zinc-300 hover:bg-zinc-50"
-  >
-    Sign up
-  </a>
 </header>
 
       {/* Hero */}
