c758e372067a70a78cdc87933944ba0233e8951f
nsriv531 <nsriv531@mtroyal.ca>
2026-02-14 19:44:56 -0700

    changed protected access

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,14 @@
+import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+
+export default async function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = await auth();
+
+  if (!userId) notFound();
+
+  return <>{children}</>;
+}
